c7ded25eb142643a50f5c67cb7e09ea40805734e
DAYDE Emile <daydeemile@gmail.com>
2026-03-18 11:23:07 +0100

    add supplier controller and attributes fix

--- WineshopManagerStarterKit/Controllers/SuppliersController.cs
+++ WineshopManagerStarterKit/Controllers/SuppliersController.cs
@@ -18,45 +18,45 @@ public class SuppliersController : ControllerBase
 
     // GET: api/wines
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Supplier>>> GetAll()
+    public async Task<ActionResult<IEnumerable<Suppliers>>> GetAll()
     {
-        return await _context.Supplier.ToListAsync();
+        return await _context.Suppliers.ToListAsync();
     }
 
     // GET: api/wines/5
     [HttpGet("{id}")]
-    public async Task<ActionResult<Supplier>> GetById(int id)
+    public async Task<ActionResult<Suppliers>> GetById(int id)
     {
-        var supplier = await _context.Supplier.FindAsync(id);
+        var suppliers = await _context.Suppliers.FindAsync(id);
 
-        if (supplier == null)
+        if (suppliers == null)
         {
             return NotFound();
         }
 
-        return supplier;
+        return suppliers;
     }
 
     // POST: api/wines
     [HttpPost]
-    public async Task<ActionResult<Supplier>> Create(Supplier supplier)
+    public async Task<ActionResult<Suppliers>> Create(Suppliers suppliers)
     {
-        _context.Supplier.Add(supplier);
+        _context.Suppliers.Add(suppliers);
         await _context.SaveChangesAsync();
 
-        return CreatedAtAction(nameof(GetById), new { id = supplier.Id }, supplier);
+        return CreatedAtAction(nameof(GetById), new { id = suppliers.Id }, suppliers);
     }
 
     // PUT: api/wines/5
     [HttpPut("{id}")]
-    public async Task<IActionResult> Update(int id, Supplier supplier)
+    public async Task<IActionResult> Update(int id, Suppliers suppliers)
     {
-        if (id != supplier.Id)
+        if (id != suppliers.Id)
         {
             return BadRequest();
         }
 
-        _context.Entry(supplier).State = EntityState.Modified;
+        _context.Entry(suppliers).State = EntityState.Modified;
 
         try
         {
@@ -64,7 +64,7 @@ public class SuppliersController : ControllerBase
         }
         catch (DbUpdateConcurrencyException)
         {
-            if (!await _context.Supplier.AnyAsync(s => s.Id == id))
+            if (!await _context.Suppliers.AnyAsync(s => s.Id == id))
             {
                 return NotFound();
             }
@@ -79,14 +79,14 @@ public class SuppliersController : ControllerBase
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(int id)
     {
-        var supplier = await _context.Supplier.FindAsync(id);
+        var suppliers = await _context.Suppliers.FindAsync(id);
 
-        if (supplier == null)
+        if (suppliers == null)
         {
             return NotFound();
         }
 
-        _context.Supplier.Remove(supplier);
+        _context.Suppliers.Remove(suppliers);
         await _context.SaveChangesAsync();
 
         return NoContent();
