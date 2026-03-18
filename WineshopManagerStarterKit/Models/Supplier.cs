452a82f3c9e6482e26e56c5ea0d5fcfd5eb6dff6
DAYDE Emile <daydeemile@gmail.com>
2026-03-18 11:56:59 +0100

    add supplier controller and attributes fix

--- WineshopManagerStarterKit/Models/Supplier.cs
+++ WineshopManagerStarterKit/Models/Supplier.cs
@@ -3,7 +3,7 @@ namespace WineshopManagerStarterKit.Models;
 public class Supplier
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string StreetNumber { get; set; }
     public string PostalCode { get; set; }
     public string City { get; set; }
